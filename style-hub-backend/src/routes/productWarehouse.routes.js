5cde15f4a22d090c23842e73e15c387b7a4afdf1
alvaroescudero7 <alvaro.escudero@unmsm.edu.pe>
2025-01-29 18:53:34 -0500

    2do Commit: Especificación Bug Fantasma del Código

--- style-hub-backend/src/routes/productWarehouse.routes.js
+++ style-hub-backend/src/routes/productWarehouse.routes.js
@@ -1,8 +1,11 @@
+/* Código que se considera 'Bug Fantasma' porque no lo consume el módulo frontend */
+
 import { Router } from "express";
 import { ProductWarehouseController } from "../controllers/productWarehouse.controller.js";
 
 const router = Router();
 
+/* Rutas creadas para vistas dirigidas al administrador de la página pero no se ha implementado en el módulo del frontend */
 router.get("/", ProductWarehouseController.getProducts);
 router.get("/:id", ProductWarehouseController.getProduct);
 router.post("/", ProductWarehouseController.createProduct);
